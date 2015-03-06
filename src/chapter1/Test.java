0c3c9d52a04e280c5c03f1cd58aa9fd3611254be
Ananth Majumdar <ananth.majumdar@gmail.com>
2015-03-05 21:37:14 -0500

    test

--- src/chapter1/Test.java
+++ src/chapter1/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	
 	public static void main(String[] args) {
-		int[] arr = new int[100];
-		System.out.println(arr[2]);
+		
+		System.out.println(((10.00f - 9.99f)*100)/100);
 	}
 }
