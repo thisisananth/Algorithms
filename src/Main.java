fde4c1849f0443488cf607ff2a23162cf108bca3
Ananth Majumdar <ananth.majumdar@gmail.com>
2015-03-12 20:20:08 -0400

    Revert "Revert "new files""
    This reverts commit d7f8a8da3d1f0a180e1fe49feb6257dd0127bf75.

--- src/Main.java
+++ src/Main.java
@@ -52,40 +52,7 @@ class myStuff implements Runnable {
 		// Your program here
 
 
-		Scanner scanner = new Scanner(System.in);
-
-		try {
-			List<String> expenses = null;
-			while (scanner.hasNext()) {
-				
-				
-				int numItems = scanner.nextInt();
-			
-				
-				if(numItems!=0){
-					expenses = new ArrayList<String>();
-					for(int i=0;i<numItems;i++){
-						expenses.add(scanner.next());
-					}
-				int totalExchangedCents = calculateTotalExchange(expenses);
-				if(totalExchangedCents%100 < 10){
-					System.out.println("$"+totalExchangedCents/100+"."+"0"+totalExchangedCents%100);
-				}else{
-					System.out.println("$"+totalExchangedCents/100+"."+totalExchangedCents%100);
-				}
-					
-				
-				}else{
-					return;
-				}
-				
-				
-
-			}
-		} catch (InputMismatchException e) {
-			e.printStackTrace();
-		}
-
+		
 	
 
 	}
@@ -93,41 +60,7 @@ class myStuff implements Runnable {
 	// You can insert more classes here if you want.
 
 
-	public static int calculateTotalExchange(List<String> expenses){
-		
-		long totalCents = 0;
-		int[] centArray = new int[expenses.size()];
-		for(int i=0;i<expenses.size();i++){
-			String expense = expenses.get(i);
-			String[] dollarsAndCents = expense.split("\\.");
-			int cents = Integer.valueOf(dollarsAndCents[0])*100;
-			if(dollarsAndCents.length >1){
-				cents = cents + Integer.valueOf(dollarsAndCents[1]);
-			}
-			centArray[i] = cents;
-			totalCents +=cents;
-		}
-		
-		double average = Math.round((double)totalCents/expenses.size());
-		
-		
-		int totalTaken = 0;
-		int totalGiven = 0;
-		
-		for(int i=0;i<centArray.length;i++){
-			
-			if(centArray[i]<average){
-				totalTaken += average - centArray[i];
-			}else if(centArray[i]>average){
-				totalGiven += centArray[i] - average;
-			}
-		}
-		
-		return  totalTaken > totalGiven ? totalGiven : totalTaken;
-		
-			
-		
-		}
+	
 	
 	
 }
