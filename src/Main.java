10db978a1c88c19e727f8030691941506d9ca8f5
Ananth Majumdar <ananth.majumdar@gmail.com>
2015-03-18 07:33:18 -0400

    updated PrimaryArithmetic problem solved

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
-package chapter2;
-
-import java.util.Scanner;
-
-/**
- * Created by ananthmajumdar on 3/17/15.
+/*
+ * Main.java
+ *  java program model for www.programming-challenges.com
  */
-public class PrimaryArithmetic {
 
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
+
+class Main implements Runnable{
+    static String ReadLn(int maxLength){  // utility function to read from stdin,
+        // Provided by Programming-challenges, edit for style only
+        byte line[] = new byte [maxLength];
+        int length = 0;
+        int input = -1;
+        try{
+            while (length < maxLength){//Read untill maxlength
+                input = System.in.read();
+                if ((input < 0) || (input == '\n')) break; //or untill end of line ninput
+                line [length++] += input;
+            }
+
+            if ((input < 0) && (length == 0)) return null;  // eof
+            return new String(line, 0, length);
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    public static void main(String args[])  // entry point from OS
+    {
+        Main myWork = new Main();  // Construct the bootloader
+        myWork.run();            // execute
+    }
+
+    public void run() {
+        new myStuff().run();
+    }
+}
+class myStuff implements Runnable{
+    public void run(){
 
         Scanner scanner = new Scanner(System.in);
 
@@ -29,17 +59,12 @@ public class PrimaryArithmetic {
 
 
                 while(true){
-                    /*System.out.println("quot1:"+quot1);
-                    System.out.println("quot2:"+quot2);
-                    System.out.println("rem1:"+rem1);
-                    System.out.println("rem2:"+rem2);
-                    System.out.println("carryCount:"+carryCount);
-                    System.out.println("currentCarry:"+currentCarry);*/
+
                     if(quot1!=0 && quot2!=0){
                         rem1 = quot1%10;
                         rem2 = quot2%10;
                         if(rem1+rem2+currentCarry>9){
-                               carryCount+=1;
+                            carryCount+=1;
                             currentCarry=1;
                         }else{
                             currentCarry=0;
@@ -82,4 +107,6 @@ public class PrimaryArithmetic {
         }
 
     }
+
+    // You can insert more classes here if you want.
 }
