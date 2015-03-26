5a72f10043f6418f4f26f12c01a33536785ab9d8
Ananth Majumdar <ananth.majumdar@gmail.com>
2015-03-26 07:57:41 -0400

    Main for PC

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+/*
+ * Main.java
+ *  java program model for www.programming-challenges.com
+ */
+
+import java.io.*;
+import java.util.*;
+
+class Main implements Runnable{
+    static String ReadLn(int maxLength){  // utility function to read from stdin,
+        // Provided by Programming-challenges, edit for style only
+        byte line[] = new byte [maxLength];
+        int length = 0;
+        int input = -1;
+        try{
+            while (length < maxLength){//Read untill maxlength
+                input = System.in.read();
+                if ((input < 0) || (input == '\n')) break; //or untill end of line ninput
+                line [length++] += input;
+            }
+
+            if ((input < 0) && (length == 0)) return null;  // eof
+            return new String(line, 0, length);
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    public static void main(String args[])  // entry point from OS
+    {
+        Main myWork = new Main();  // Construct the bootloader
+        myWork.run();            // execute
+    }
+
+    public void run() {
+        new myStuff().run();
+    }
+}
+class myStuff implements Runnable{
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String nextLine = scanner.nextLine();
+            if (nextLine.trim().equals("0")) {
+                return;
+            } else {
+                long n = Long.parseLong(nextLine);
+                if(Math.floor(Math.sqrt(n))==Math.ceil(Math.sqrt(n))){
+                    System.out.println("yes");
+                }else{
+                    System.out.println("no");
+                }
+            }
+        }
+
+    }
+
+    // You can insert more classes here if you want.
+}
